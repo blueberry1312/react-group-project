0743d48227749e7541ad4034038bbf25e82358d4
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-04-13 19:29:07 -0400

    Display joined missions in My Profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './MyProfile.css';
 
 const MyProfile = () => {
   const rockets = useSelector((state) => state.rockets.rockets.filter((rocket) => rocket.reserved));
+  const missions = useSelector(
+    (state) => state.missions.missions.filter((mission) => mission.reserved),
+  );
 
   return (
-    <div>
-      <p>Profile page</p>
-      <div className="Rockets">
-        {rockets.map((rocket) => (
-          <div key={rocket.id} className="Rockets-container">
-            <p>{rocket.name}</p>
-          </div>
-        ))}
+    <div className="MyProfile">
+      <div className="missions-list">
+        <h2 className="h2-myprofile">My Missions</h2>
+        <table className="table-missions">
+          <tbody>
+            {missions.map((mission) => (
+              <tr key={mission.mission_id}>
+                <td className="td-missions">{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="rockets-list">
+        <h2 className="h2-myprofile">My Rockets</h2>
+        <table className="table-rockets">
+          <tbody>
+            {rockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td className="td-rockets">{rocket.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { fetchRockets } from './redux/rockets/rocketsSlice';
+import { fetchMissions } from './redux/missions/missionsSlice';
 import store from './redux/store';
 
 store.dispatch(fetchRockets());
+store.dispatch(fetchMissions());
 
 ReactDOM.render(
   <React.StrictMode>
