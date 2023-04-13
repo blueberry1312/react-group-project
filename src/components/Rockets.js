2057e851b23987bde62fe50a1701b03506080ac5
Kola Kolade <kolakolade@gmail.com>
2023-04-13 18:40:40 +0100

    Implement rocket booking and cancellation

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { bookRocket, fetchRockets } from '../redux/rockets/rocketsSlice';
+import {
+  bookRocket,
+  cancelBooking,
+  fetchRockets,
+} from '../redux/rockets/rocketsSlice';
 import './rockets.css';
 
 const Rockets = () => {
@@ -15,6 +19,10 @@ const Rockets = () => {
 
   const handleBookRocket = (rocketId) => {
     dispatch(bookRocket(rocketId));
+  };
+
+  const handleCancelBooking = (rocketId) => {
+    dispatch(cancelBooking(rocketId));
   };
 
   if (isLoading) {
@@ -40,23 +48,40 @@ const Rockets = () => {
 
   return (
     <div className="Rockets">
-      {rockets.map((rocket) => (
-        <div key={rocket.id} className="Rockets-container">
-          <img
-            src={rocket.flickr_images[0]}
-            alt={rocket.name}
-            className="Rocket-img"
-          />
+      {rockets.map((rocket) => {
+        const isBooked = rocket.reserved;
+        const handleClick = isBooked ? handleCancelBooking : handleBookRocket;
+        const badgeDisplayed = isBooked
+          ? 'Rocket-display-badge'
+          : 'Rocket-no-display-badge';
+        {
+          /* const badgeDisplayed = isBooked ? rocket.reserved : false; */
+        }
 
-          <div className="Rockets-content">
-            <h2>{rocket.name}</h2>
-            <p>{rocket.description}</p>
-            <button type="submit" className="Rockets-button">
-              Reserve Rocket
-            </button>
+        return (
+          <div key={rocket.id} className="Rockets-container">
+            <img
+              src={rocket.flickr_images[0]}
+              alt={rocket.name}
+              className="Rocket-img"
+            />
+            <div className="Rockets-content">
+              <h2>{rocket.name}</h2>
+              <p>
+                <span className={badgeDisplayed}>Booked</span>
+                {rocket.description}
+              </p>
+              <button
+                type="submit"
+                className={isBooked ? 'cancel-button' : 'book-button'}
+                onClick={() => handleClick(rocket.id)}
+              >
+                {isBooked ? 'Cancel Reservation' : 'Reserve Rocket'}
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
