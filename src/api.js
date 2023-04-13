df7e34fd9ac5254de4a24ea3e88eadd1b8894442
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-04-12 23:16:17 -0400

    Add join/leave actions with buttons and status column

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const spaceService = {
   async getMissions() {
     const response = await axios.get('https://api.spacexdata.com/v3/missions');
-    return response.data;
+    const missions = response.data.map((mission) => ({ ...mission, reserved: false }));
+    return missions;
   },
   async getRockets() {
     try {
